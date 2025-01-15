6b3f74eab1b9242d95ce7718c82e0c97e31acb50
Filip <filipdropsik11@o2.pl>
2025-01-15 09:10:29 +0100

    Wordset functionality updated

--- src/app/Components/wordsetcreator/wordsetcreator.component.ts
+++ src/app/Components/wordsetcreator/wordsetcreator.component.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { toObservable } from '@angular/core/rxjs-interop';
 
+
 @Component({
   selector: 'app-wordsetcreator',
   standalone: true,
@@ -15,13 +16,19 @@ import { toObservable } from '@angular/core/rxjs-interop';
 export class WordsetcreatorComponent {
 
   currentUser: any;
-  wordsetValue: any;
+  wordsetObject: any;
   
   wordset = input<any>();
   wordset$ = toObservable(this.wordset)
 
   manualEditorText: string = '';
   assistedCreatorContent: Array<any> = [];
+
+  tab: string = 'assisted';
+
+  newQuestionType: string = '';
+  newQuestionValueOne: string = '';
+  newQuestionValueTwo: string = '';
 
   constructor(private authService: AuthService) {}
 
@@ -30,11 +37,45 @@ export class WordsetcreatorComponent {
       this.currentUser = user;
     });     
     this.wordset$.subscribe(value => {
-      this.wordsetValue = value
-      console.log(value);
-    })
+      this.wordsetObject = value
+      this.manualEditorText = JSON.stringify(this.wordsetObject.wordlist, null, 0);
+      this.assistedCreatorContent = JSON.parse(this.manualEditorText);
+      
+    })  
   }
 
-  
+  tabSelector(value: string) {
+    this.tab = value;
+  }
+
+  addNewQuestionAssist() {
+    try {
+      this.assistedCreatorContent.push({
+        type: this.newQuestionType,
+        valueOne: this.newQuestionValueOne,
+        valueTwo: this.newQuestionValueTwo
+      })
+      this.manualEditorText = JSON.stringify(this.assistedCreatorContent);
+      this.authService.updateWordset(this.wordsetObject, this.wordsetObject.name, this.wordsetObject.icon, this.wordsetObject.language, this.assistedCreatorContent)
+      console.log('wordset updated successfully');
+    } catch (error) {
+      console.error('failed to update wordset', error);
+    }
+  }
+
+  addnewQuestionManual() {
+    try {
+      this.assistedCreatorContent = JSON.parse(this.manualEditorText);
+      try {
+        this.authService.updateWordset(this.wordsetObject, this.wordsetObject.name, this.wordsetObject.icon, this.wordsetObject.language, this.assistedCreatorContent)  
+        console.log('wordset updated successfully');
+      } catch (error) {
+        console.log('failed to update wordset', error);
+      }
+    } catch (error) {
+      console.log('failed to save the changes', error);
+      alert("{{'SAVEFAILED' | translate}}")
+    }
+  }
 
 }
